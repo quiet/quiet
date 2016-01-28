fb8492eb15c6b3431d93b6bcbe9cbf6df98c76f8
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-01-27 18:45:20 -0800

    now it's a pointer

--- javascripts/transmit.js
+++ javascripts/transmit.js
@@ -3,7 +3,7 @@ var SampleEncoder = SampleEncoder || {};
 var Module = {
     onFileRead: function(e) {
         var payload = allocate(intArrayFromString(e.target.result), 'i8', ALLOC_NORMAL);
-        ccall('encoder_set_payload', 'number', ['pointer', 'array', 'number'], [Module.encoder, payload, e.target.result.length]);
+        ccall('encoder_set_payload', 'number', ['pointer', 'pointer', 'number'], [Module.encoder, payload, e.target.result.length]);
 
         var sample_len = 16384;
         var samples = ccall('malloc', 'pointer', ['number'], [4 * sample_len]);
