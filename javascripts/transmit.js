4e4314eaa018f9d9f4d7853646a714630eecd73a
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-01-27 23:26:07 -0800

    make send happen on button press
    maybe this will make ios safari work?

--- javascripts/transmit.js
+++ javascripts/transmit.js
@@ -22,8 +22,7 @@ var Module = {
                 }
             }
         };
-
-        transmitter.connect(Module.audio_ctx.destination);
+        document.querySelect('[data-quiet-send-button]').addEventListener('click', function() { transmitter.connect(Module.audio_ctx.destination) }, false);
     },
     onFileSelect: function(e) {
         var reader = new FileReader()
