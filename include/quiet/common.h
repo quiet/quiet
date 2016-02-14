4296468946d2a1cd28fdf32336be155f04c1ee2d
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-02-13 17:26:16 -0800

    separate out create_ofdm_subcarriers

--- include/quiet/common.h
+++ include/quiet/common.h
@@ -3,4 +3,5 @@
 #include "quiet.h"
 
 static const unsigned int SAMPLE_RATE = 44100;
+unsigned char *create_ofdm_subcarriers(const ofdm_options *opt);
 #endif  // QUIET_COMMON_H
--- src/encode.c
+++ src/encode.c
@@ -12,32 +12,6 @@ void decoder_opt_set_sample_rate(decoder_options *opt, float sample_rate) {
     opt->sample_rate = sample_rate;
 }
 
-unsigned char *create_ofdm_subcarriers(const ofdm_options *opt) {
-    unsigned char *subcarriers =
-        malloc(opt->num_subcarriers * sizeof(unsigned char));
-    // get the default subcarrier placement and then modify it slightly
-    ofdmframe_init_default_sctype(opt->num_subcarriers, subcarriers);
-    // now add some nulls
-    size_t left_end = opt->num_subcarriers / 2;
-    while (subcarriers[left_end] == OFDMFRAME_SCTYPE_NULL) {
-        left_end--;
-    }
-    size_t right_end = opt->num_subcarriers / 2;
-    while (subcarriers[right_end] == OFDMFRAME_SCTYPE_NULL) {
-        right_end++;
-    }
-    // n.b. confusingly the left part of the array corresponds to the right band
-    // and vice versa
-    for (size_t i = 0; i < opt->right_band; i++) {
-        subcarriers[left_end - i] = OFDMFRAME_SCTYPE_NULL;
-    }
-    for (size_t i = 0; i < opt->left_band; i++) {
-        subcarriers[right_end + i] = OFDMFRAME_SCTYPE_NULL;
-    }
-
-    return subcarriers;
-}
-
 modulator *create_modulator(const modulator_options *opt) {
     if (!opt) {
         return NULL;
