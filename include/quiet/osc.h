cb065ff0253d78a9d68689179421511bf98e1843
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-05-24 12:21:25 -0700

    remove osc experiment

--- include/quiet/osc.h
+++
@@ -1,7 +0,0 @@
-#include "quiet/common.h"
-#include <math.h>
-
-osc *osc_create(float freq);
-void osc_destroy(osc *o);
-void osc_mix_down_16384(osc *o, float *input, float complex *output);
-void osc_mix_down(osc *o, float *input, float complex *output, size_t n);
