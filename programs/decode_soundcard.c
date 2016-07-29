80acd3394d03831060ef34894d4e677cd13e5dd6
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-07-29 16:52:26 -0700

    flush on every write in decode_soundcard
    this is not ideal, but it will help us see if this is working in the
    testbench

--- programs/decode_soundcard.c
+++ programs/decode_soundcard.c
@@ -27,6 +27,7 @@ int decode_from_soundcard(FILE *output, quiet_decoder_options *opt) {
                 break;
             }
             fwrite(write_buffer, 1, read, output);
+            fflush(output);
         }
     }
 
