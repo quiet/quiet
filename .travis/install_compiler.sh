a76033a2f6d8a8c3e7defdd1d8679328f5638f24
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-07-29 14:30:40 -0700

    get alsa-utils

--- .travis/install_compiler.sh
+++ .travis/install_compiler.sh
@@ -5,5 +5,5 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update
     sudo apt-get install -y gcc-4.9 g++-4.9
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
-    sudo apt-get install libasound-dev
+    sudo apt-get install libasound-dev alsa-utils
 fi
