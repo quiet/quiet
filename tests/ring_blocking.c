fded77aedc51c23d1315f004dc5b11165022dcb6
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2017-08-27 18:24:19 -0700

    pthread-like for win

--- tests/ring_blocking.c
+++ tests/ring_blocking.c
@@ -2,7 +2,6 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <pthread.h>
 #include <time.h>
 
 #ifdef _MSC_VER
@@ -14,8 +13,51 @@ static inline void usleep(int micros)
     }
     return Sleep(micros/1000);
 }
+
+typedef DWORD thread_id;
+
+static void thread_create(thread_id *id, uint32_t (*fn)(void*), void *arg) {
+    CreateThread(NULL, NULL, fn, arg, 0, id);
+}
+
+static uint32_t thread_join(thread_id id) {
+    return WaitForSingleObject(id, INFINITE);
+}
 #else
 #include <unistd.h>
+#include <pthread.h>
+
+typedef pthread_t thread_id;
+
+typedef struct {
+    uint32_t (*fn)(void*);
+    void *arg;
+} thread_call;
+
+static void *thread_wrapper(void *callv) {
+    thread_call *call = (thread_call*)callv;
+    uint32_t *res = (uint32_t*)malloc(sizeof(uint32_t));
+    *res = call->fn(call->arg);
+    free(call);
+    pthread_exit(res);
+    return NULL;
+}
+
+static void thread_create(thread_id *id, uint32_t (*fn)(void*), void *arg) {
+    thread_call *call = (thread_call*)malloc(sizeof(thread_call));
+    call->fn = fn;
+    call->arg = arg;
+    pthread_create(id, NULL, thread_wrapper, call);
+}
+
+static uint32_t thread_join(thread_id id) {
+    void *res_p;
+    pthread_join(id, &res_p);
+    uint32_t *res = (uint32_t*)res_p;
+    uint32_t res_temp = *res;
+    free(res);
+    return res_temp;
+}
 #endif
 
 typedef struct {
@@ -26,15 +68,14 @@ typedef struct {
 
 const uint8_t seq_len = 231; // make this non power of 2 to force buffer variations
 
-void *write_sequence(void *arg_void) {
+uint32_t write_sequence(void *arg_void) {
     arg_t *arg = (arg_t*)arg_void;
     ring *buf = arg->buf;
     size_t write_len = arg->write_len;
     uint8_t seq = 0;
     size_t temp_len = 16;
     uint8_t *temp = (uint8_t*)malloc(temp_len * sizeof(uint8_t));
-    int *res = (int*)malloc(1 * sizeof(int));
-    *res = 0;
+    uint32_t res = 0;
     for (size_t i = 0; i < write_len; ) {
         size_t nitems = rand() % 16 + 1;
         if (i + nitems > write_len) {
@@ -42,7 +83,7 @@ void *write_sequence(void *arg_void) {
         }
         for (size_t j = 0; j < nitems; j++) {
             temp[j] = seq;
-            *res += seq;
+            res += seq;
             seq++;
             seq %= seq_len;
         }
@@ -62,19 +103,17 @@ void *write_sequence(void *arg_void) {
         i += nitems;
     }
     free(temp);
-    pthread_exit(res);
-    return NULL;
+    return res;
 }
 
-void *read_sequence(void *arg_void) {
+uint32_t read_sequence(void *arg_void) {
     arg_t *arg = (arg_t*)arg_void;
     ring *buf = arg->buf;
     size_t write_len = arg->write_len;
     size_t temp_len = 16;
     uint8_t *temp = (uint8_t*)malloc(temp_len * sizeof(uint8_t));
     uint8_t seq = 0;
-    int *res = (int*)malloc(1 * sizeof(int));
-    *res = 0;
+    uint32_t res = 0;
     for (size_t i = 0; i < write_len; ) {
         size_t nitems = rand() % 16 + 1;
         if (i + nitems > write_len) {
@@ -95,14 +134,12 @@ void *read_sequence(void *arg_void) {
         }
         for (size_t j = 0; j < nitems; j++) {
             if (arg->multi) {
-                *res += temp[j];
+                res += temp[j];
             } else {
                 if (temp[j] != seq) {
                     printf("mismatch at %zu: %u != %u\n", i + j, temp[j], seq);
                     free(temp);
-                    *res = 1;
-                    pthread_exit(res);
-                    return NULL;
+                    return 1;
                 }
             }
             seq++;
@@ -111,33 +148,27 @@ void *read_sequence(void *arg_void) {
         i += nitems;
     }
     free(temp);
-    pthread_exit(res);
-    return NULL;
+    return res;
 }
-
 
 int main() {
     srand(time(NULL));
     ring *buf = ring_create(1 << 14);
-    pthread_t w, w1, r, r1;
+    thread_id w, w1, r, r1;
 
     // first we do a test with a single writer, single reader
     // in this test, the reader will ensure the sequence appears
     // strictly in the same order it is written
     arg_t args = {
-        .buf = buf,
-        .write_len = 1 << 22,
-        .multi = false,
+        buf,
+        1 << 22,
+        false,
     };
-    pthread_create(&w, NULL, write_sequence, &args);
-    pthread_create(&r, NULL, read_sequence, &args);
+    thread_create(&w, write_sequence, &args);
+    thread_create(&r, read_sequence, &args);
 
-    pthread_join(w, NULL);
-    int res;
-    void *res_p;
-    pthread_join(r, &res_p);
-    res = *(int*)res_p;
-    free(res_p);
+    thread_join(w);
+    uint32_t res = thread_join(r);
 
     printf("single reader, single writer test passed: %s\n", res ? "FALSE" : "TRUE");
 
@@ -145,25 +176,17 @@ int main() {
     // we relax the sequence restriction and now just look for the same sums
     args.multi = true;
 
-    pthread_create(&w, NULL, write_sequence, &args);
-    pthread_create(&w1, NULL, write_sequence, &args);
-    pthread_create(&r, NULL, read_sequence, &args);
-    pthread_create(&r1, NULL, read_sequence, &args);
+    thread_create(&w, write_sequence, &args);
+    thread_create(&w1, write_sequence, &args);
+    thread_create(&r, read_sequence, &args);
+    thread_create(&r1, read_sequence, &args);
 
-    int write_sum = 0;
-    pthread_join(w, &res_p);
-    write_sum += *(int*)res_p;
-    free(res_p);
-    pthread_join(w1, &res_p);
-    write_sum += *(int*)res_p;
-    free(res_p);
-    int read_sum = 0;
-    pthread_join(r, &res_p);
-    read_sum += *(int*)res_p;
-    free(res_p);
-    pthread_join(r1, &res_p);
-    read_sum += *(int*)res_p;
-    free(res_p);
+    uint32_t write_sum = 0;
+    write_sum += thread_join(w);
+    write_sum += thread_join(w1);
+    uint32_t read_sum = 0;
+    read_sum += thread_join(r);
+    read_sum += thread_join(r1);
 
     printf("2 reader, 2 writer test passed: %s\n", (read_sum != write_sum) ? "FALSE" : "TRUE");
     res = res ? res : read_sum != write_sum;
